5914d42ee0e4a089e3aba3719b9801bc61f6fc93
Firsoova08 <foxalishca@mail.ru>
2026-02-01 23:50:48 +0300

    добавление группы и даты

--- server/program.cs
+++ server/program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("   ЛАБОРАТОРНАЯ РАБОТА №4");
-string name="Фирсова Алиса и Алексеева Валерия"
+string name="Фирсова Алиса и Алексеева Валерия";
+string group = "ИСП-232";
+ Console.WriteLine($"ФИО: {name}");
+  Console.WriteLine($"Группа: {group}");
+ Console.WriteLine($"Дата: {DateTime.Now}");
+ 

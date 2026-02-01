46e514b547dd3f515218482767be439114a2ad69
Firsoova08 <foxalishca@mail.ru>
2026-02-01 23:53:56 +0300

    добавление логики выбора

--- server/program.cs
+++ server/program.cs
@@ -15,4 +15,19 @@ string group = "ИСП-232";
             Console.WriteLine("2 - Показать группу");
             Console.WriteLine("3 - Показать дату");
             Console.WriteLine("4 - Выход");
+            string choice = Console.ReadLine();
+            
+            if (choice == "1")
+                Console.WriteLine("Фирсова Алексеева А.И.");
+            else if (choice == "2")
+                Console.WriteLine("ИСРПО-01-22");
+            else if (choice == "3")
+                Console.WriteLine(DateTime.Now);
+            else if (choice == "4")
+                run = false;
+            else
+                Console.WriteLine("Ошибка: выберите 1-4");
+                
+            Console.WriteLine();
         }
+        
